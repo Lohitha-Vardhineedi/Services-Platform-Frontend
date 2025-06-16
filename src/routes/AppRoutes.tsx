758034dfb808c19af3b4a41d4dd5c4b6c6086247
Uday Kalyan <udaykalyan975@gmail.com>
2025-06-16 14:49:48 +0530

    routes path change

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -10,8 +10,6 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
-      <Route path="/services" element={<ServicesPage />} />
-      <Route path="/contact" element={<ContactPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
