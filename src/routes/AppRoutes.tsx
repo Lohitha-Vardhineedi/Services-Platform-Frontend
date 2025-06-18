63da4c63b0600e9b10ec16dd8a62d26e81485626
lohitha <lohitha.99630@gmail.com>
2025-06-18 16:59:24 +0530

    updates

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -18,20 +18,20 @@ import KeyFeaturesPage from '../pages/KeyFeaturesPage';
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<HomePage />}/>
             <Route path="/about" element={<AboutPage />} />
             <Route element={<AuthLayout />}>
                 <Route path="/login/user" element={<UserLogin />} />
-                <Route path="/login/technician" element={<TechnicianLogin />} />
-                <Route path="/signup/user" element={<UserSignup />} />
-                <Route path="/signup/technician" element={<TechnicianSignup />} />
-            </Route>    
-            <Route path="/subscription" element={<SubscriptionPage />} />
+                <Route path="/login/technician" element={<TechnicianLogin />}/>
+                <Route path="/signup/user" element={<UserSignup />}/>
+                <Route path="/signup/technician" element={<TechnicianSignup />}/>
+            </Route>   
+            <Route path="/subscription" element={<SubscriptionPage/>}/>
             <Route path="/franchise" element={<FranchisePage />} />
             <Route path="/plans/:id" element={<PlanDetailsPage />} />
             <Route path="/comingsoon" element={<ComingSoonPage />} />
-            <Route path="/categories" element={<CategoriesPage />} />
-            <Route path="/features" element={<KeyFeaturesPage />} />
+            <Route path="/categories" element={<CategoriesPage />}/>
+            <Route path="/features" element={<KeyFeaturesPage />}/>
             <Route path="*" element={<NotFoundPage />} />
         </Routes>
     );
