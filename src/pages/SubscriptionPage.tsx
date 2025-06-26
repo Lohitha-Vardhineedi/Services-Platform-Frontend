6cd34a40550aef291ae9cb66a2baba3958ade0c9
Uday Kalyan <udaykalyan975@gmail.com>
2025-06-26 10:50:37 +0530

    subscription style change

--- src/pages/SubscriptionPage.tsx
+++ src/pages/SubscriptionPage.tsx
@@ -46,8 +46,8 @@ const SubscriptionPage: React.FC = () => {
         </div>
 
         {/* Plans Grid */}
-        {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 xl:gap-10"> */}
-          <div className="flex flex-col gap-6 sm:gap-8 xl:gap-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 xl:gap-10"> 
+          {/* <div className="flex gap-6 sm:gap-8 xl:gap-10"> */}
           {plans.map((plan: Plan) => {
             const IconComponent = plan.icon;
             return (
