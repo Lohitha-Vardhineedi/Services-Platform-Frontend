6340ea59ec8461c8ac728c7cae3ac3910921f044
lohitha <lohitha.99630@gmail.com>
2025-07-07 15:57:30 +0530

    changes

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -59,7 +59,7 @@ const LoginForm: React.FC<LoginFormProps> = ({ defaultRole = 'user' }) => {
         localStorage.setItem('userId', response.result.id);
 
         if (role === 'technician') {
-          navigate('/technician');
+          navigate('/technician/dashboard');
         } else {
           navigate('/')
         }
