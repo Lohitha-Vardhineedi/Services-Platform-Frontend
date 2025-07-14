7cf971879d4badb5a817bec2fc824e5fc61000e5
tagoor-tag <tagoorbobbillapati02@gmail.com>
2025-07-14 09:19:08 +0530

    changes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Download, Menu, X, ShoppingCart, Star } from 'lucide-react';
 import { TbLogout } from "react-icons/tb";
-import CompanyReviewModel from '../services/CompanyReviewModel'
+import CompanyReviewModel from '../homepage/CompanyReviewModel'
 
 interface User {
   username: string;
