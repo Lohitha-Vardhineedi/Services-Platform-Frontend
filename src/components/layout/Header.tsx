05b8df237f45d05ead6046575f19f85c3654cbbb
lohitha <lohitha.99630@gmail.com>
2025-10-27 10:54:41 +0530

    updates

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -103,7 +103,7 @@ function Header() {
             <button onClick={handleLogoClick}>
               <img
                 src="https://old.prnvservices.com/uploads/logo/1695377568_logo-white.png"
-                alt="PRNV Logo"
+                alt="PRNV"
                 className="h-8 w-auto"
               />
             </button>
